11afafc32814171760194f899c396ee901d7fc19
ritesh-appsinfinito <ritesh.gupta@appsinfinito.com>
2024-01-04 17:08:24 +0530

    created pages

--- marketplace/app/page.js
+++ marketplace/app/page.js
@@ -1,8 +1,13 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page 1</div>
+    <>
+    <Header />
+    <Hero />
+  </>
   )
 } 
 
